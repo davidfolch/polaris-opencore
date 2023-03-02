9ddba88fc690f8c0acb92682254b4b1e4a1eb104
David Folch Agulles <davidfolch94@gmail.com>
2023-03-03 00:23:00 +0100

    CHORE(Motion): return to arm treshold 300

--- libraries/CoreMotion/CoreMotion.h
+++ libraries/CoreMotion/CoreMotion.h
@@ -66,7 +66,7 @@ class CoreMotion: public Machine {
   static constexpr int SWING_THRESHOLD_HIGH = 300;
   static constexpr int ROLL_SPEED_THRESHOLD_LOW = 40;
   static constexpr int ROLL_SPEED_THRESHOLD_HIGH = 150;
-  static constexpr int ARM_THRESHOLD_Z = 1000;
+  static constexpr int ARM_THRESHOLD_Z = 300;
   static constexpr int ARM_ALT_THRESHOLD_Z = 1000;
   static constexpr int ARM_THRESHOLD_XY = 100;
   static constexpr float VERTICAL_POSITION = 8.0;
